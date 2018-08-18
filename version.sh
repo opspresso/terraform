dfd0654a5b7cf6e10197b437c2ed81a5600dfba6
nalbam <me@nalbam.com>
2018-08-18 10:42:12 +0900

    log format

--- version.sh
+++ version.sh
@@ -7,7 +7,7 @@ GITHUB_TOKEN=${3}
 NOW=$(cat ./VERSION)
 NEW=$(curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest | grep tag_name | cut -d'"' -f4 | cut -c 2- | xargs)
 
-printf '# %-10s: %-10s %-10s\n' "${REPONAME}" "${NOW}" "${NEW}"
+printf '# %-10s %-10s %-10s\n' "${REPONAME}:" "${NOW}" "${NEW}"
 
 if [ "${NOW}" != "${NEW}" ]; then
     printf "${NEW}" > VERSION
